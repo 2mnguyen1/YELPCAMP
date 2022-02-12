60d0ee76273662c292e864bb0443ab0ac8284b3b
2mnguyen1 <2mnguyen1@gmail.com>
2022-02-11 18:01:59 -0800

    adding port

--- app.js
+++ app.js
@@ -157,6 +157,8 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err });
 })
 
-app.listen('3000', () => {
-    console.log('3000 PORT')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`${port} PORT`)
 })
